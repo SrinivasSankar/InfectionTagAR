0c19b2cb7522296d2b24844db896dd5e8434a7bb
SrinivasSankar <SriniSankar2006@gmail.com>
2025-12-19 16:22:22 -0500

    Initial Commit

---
+++ IRLGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IRLGame
+//
+//  Created by Srinivas Sankaranarayanan on 12/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ IRLGameTests/IRLGameTests.swift
@@ -0,0 +1,17 @@
+//
+//  IRLGameTests.swift
+//  IRLGameTests
+//
+//  Created by Srinivas Sankaranarayanan on 12/19/25.
+//
+
+import Testing
+@testable import IRLGame
+
+struct IRLGameTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
